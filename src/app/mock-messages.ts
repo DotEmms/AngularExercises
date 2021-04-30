8285b9ed0cfaa6ee16c51e836675b7f234ee9560
DotEmms <emma_danckaert@live.be>
2021-04-30 11:57:10 +0200

    changed colors

--- src/app/mock-messages.ts
+++ src/app/mock-messages.ts
@@ -1,7 +1,7 @@
 import {Message} from './message';
 
 export const MESSAGES:Message[] = [
-    {message:"This is a message", newMessage:""},
+    {message:"This is a message 1", newMessage:""},
     {message:"This is a message 2", newMessage:""},
     {message:"This is a message 3", newMessage:""},
     {message:"This is a message 4", newMessage:""},
